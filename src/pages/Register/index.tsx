eec2b9c621cb8c9885b2d000227e6361eb6bdb0b
Nícolas Medeiros Thiesen <49465248+NicolasThiesen@users.noreply.github.com>
2021-05-30 20:24:26 -0300

    Avaliação6  Changes to be committed: 	modified:   package.json 	modified:   src/pages/Login/index.tsx 	modified:   src/pages/Login/style.ts 	modified:   src/pages/Register/index.tsx 	modified:   src/pages/Register/style.ts 	modified:   yarn.lock
     Untracked files:
    	src/components/NewActivyModal/
    	src/components/NewCourseUnitModal/

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,28 +1,42 @@
 import { Link } from 'react-router-dom';
-import {Container, Content, FormContainer, InputContainer} from './style';
+import {Container, Content, FormContainer, InputContainer, Error} from './style';
 import { FiLogIn, FiMail, FiLock, FiUser, FiArrowLeft} from 'react-icons/fi';
 import { Button } from '../../components';
 import { Background } from '../Login/style';
+import { useForm } from 'react-hook-form'
+
+interface FormData {
+    name: string;
+    email: string;
+    password: string
+}
 
 export const Register = () => {
+    const { register, handleSubmit, formState: {errors}} = useForm<FormData>();
+
+    const onSubmit = handleSubmit(data => alert(JSON.stringify(data)))
+
     return (
         <Container>
             <Content>
                 <FormContainer>
                     <h2>Cadastro</h2>
-                    <form action="">
+                    <form onSubmit={onSubmit}>
                         <InputContainer>
                             <FiUser size={40}/>
-                                <input type="text" placeholder="Usuário" />
+                                <input type="text" placeholder="Usuário" {...register("name", {required:true})} />
                         </InputContainer>
+                        {errors.name && <Error>O campo usuário é obrigatório</Error>}
                         <InputContainer>
                             <FiMail size={40}></FiMail>
-                                <input type="email" placeholder="E-mail" />
+                                <input type="email" placeholder="E-mail" {...register("email", {required:true})}/>
                         </InputContainer>
+                        {errors.email && <Error>O campo email é obrigatório</Error>}
                         <InputContainer>
                             <FiLock size={40}> </FiLock>
-                                <input type="password" placeholder="Senha" />
+                                <input type="password" placeholder="Senha" {...register("password", {required:true})}/>
                         </InputContainer>
+                        {errors.password && <Error>O campo senha é obrigatório</Error>}
                         <Button type="submit">Cadastrar</Button>
                     </form>
                     <Link to="/">
